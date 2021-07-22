86d837371cf1a72bf91cf3305f7a6afca5811286
etiennezastko <etiennezastko@hotmail.fr>
2021-07-22 14:57:57 +0200

    fonction titre et paragraphe success

--- front-page.php
+++ front-page.php
@@ -3,5 +3,32 @@
 
 Landing Page
 
+<div class="container_fluid">
+
+    <div class="row"><?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <h1><?php the_title(); ?></h1>
+
+
+
+        <?php endwhile;
+                        endif; ?>
+    </div>
+    <div class="row"><?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <p>
+                    <?php the_content(); ?>
+                </p>
+        <?php endwhile;
+                        endif; ?>
+    </div>
+    <div class="row">projet</div>
+    <div class="row"></div>
+
+</div>
+
+
+
+
 
 <?php include 'footer.php'; ?>
