e390344ebb260997655f0f8a9fddfa0b918aa06d
etiennezastko <etiennezastko@hotmail.fr>
2021-08-19 16:57:44 +0200

    ajout de project

--- front-page.php
+++ front-page.php
@@ -1,32 +1,22 @@
 <?php include 'header.php'; ?>
 
 
-Landing Page
 
 <div class="container_fluid">
 
-    <div class="row"><?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+    <div class="row">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <h1><?php the_title(); ?></h1>
 
-
-
-        <?php endwhile;
-                        endif; ?>
-    </div>
-    <div class="row"><?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-                <p>
+                <section id="container">
                     <?php the_content(); ?>
-                </p>
+                </section>
+
         <?php endwhile;
-                        endif; ?>
+        endif; ?>
     </div>
 
-</div>
-<div class="row">
-    <?php include 'home.php'; ?>
-</div>
 
-<?php include 'footer.php'; ?>
+
 </div>
+<?php include 'footer.php'; ?>
